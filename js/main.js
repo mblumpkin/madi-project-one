62d8d8c1708877ebd85f77eac12ed83565929f20
mblumpkin <mblumpkin2@gmail.com>
2026-02-04 10:50:08 -0600

    Added three more bees
    Added Lauren, Hope, and Isabeela with pictures and started likes and dislikes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // JS scripts placed here
 const bees = [
   { name: "Pollen Frey", bio: "Pollen bio", img: "./img/PollenFrey.PNG",
-    likes: ["like1","like2","like3"], dislikes:["dislike1","dislike2","dislike3"], 
+    likes: ["Grapes","Wing Painting","The Show Survivor"], dislikes:["Cookie Clicker","Books with Fluff","Technology"], 
             partner: ["1", "2", "3"] },
   { name: "Beeia Honey", bio: "Beeia bio", img: "./img/BeeiaHoney.PNG",
-    likes: ["like1","like2","like3"], dislikes:["dislike1","dislike2","dislike3"], 
+    likes: ["Horses","like2","like3"], dislikes:["dislike1","dislike2","dislike3"], 
             partner: ["1", "2", "3"] },
   { name: "Burt Bee", bio: "Burt bio", img: "./img/BurtBee.PNG",
     likes: ["like1","like2","like3"], dislikes:["dislike1","dislike2","dislike3"], 
@@ -16,11 +16,20 @@ const bees = [
     likes: ["like1","like2","like3"], dislikes:["dislike1","dislike2","dislike3"], 
             partner: ["1", "2", "3"] },
   { name: "Mibee Xiao", bio: "Mibee bio", img: "./img/Mibee.PNG",
-    likes: ["like1","like2","like3"], dislikes:["dislike1","dislike2","dislike3"],
+    likes: ["like1","like2","like3"], dislikes:["The Moon","dislike2","dislike3"],
             partner: ["1", "2", "3"]  },
   {name: "Grant Buzzton", bio: "Grant bio", img: "./img/GrantBuzzton.png", 
+    likes: ["Action Movies","Rock Music","like3"], dislikes:["dislike1","dislike2","dislike3"], 
+            partner: ["1", "2", "3"] },
+  {name: "Lauren Beechelle", bio: "Lauren bio", img: "./img/LaurenBeechelle.png", 
     likes: ["like1","like2","like3"], dislikes:["dislike1","dislike2","dislike3"], 
             partner: ["1", "2", "3"] },
+  {name: "Hope Swarm", bio: "Hope bio", img: "./img/HopeSwarm.png", 
+    likes: ["like1","like2","like3"], dislikes:["dislike1","dislike2","dislike3"], 
+            partner: ["1", "2", "3"] }, 
+  {name: "Isabeela Yellolo", bio: "Isabeela bio", img: "./img/IsabeelaYelollo.png", 
+    likes: ["Fellow Bees","","like3"], dislikes:["Yellow Jackets","Gasoline","dislike3"], 
+            partner: ["1", "2", "3"] },             
 ];
 
 let index = 0;
